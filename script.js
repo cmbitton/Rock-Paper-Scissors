f65a191d96bf04c44466964daf7baf4a147f4f11
Curtis <cmbitton@gmail.com>
2022-08-30 20:40:51 -0400

    Add playRound function

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ let random_Choice = choices[Math.floor(Math.random() * 3)];
 return random_Choice;
 }
 
+function playRound(playerChoice, computerChoice) {
+    playerChoice = playerChoice.toLowerCase();
+    if (playerChoice === computerChoice) {
+        return "Tie!";
+    }
+    else if (playerChoice === 'rock' && computerChoice === 'paper' || playerChoice === 'paper' && computerChoice === 'scissors' || playerChoice === 'scissors' && computerChoice === 'rock') {
+        return `You Lose! ${computerChoice} beats ${playerChoice}`;
+    }
+    
+    else {
+        return `You Win! ${playerChoice} beats ${computerChoice}!`
+    }
+}
+
+
